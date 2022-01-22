932b75079048fdeab84f8feed73f765745352556
Ivonne <ivonne.scarlett01@gmail.com>
2022-01-21 20:47:55 -0600

    Se modifico saludo.js

--- saludo.js
+++ saludo.js
@@ -1,26 +1,34 @@
+// const params = require('yargs-parser')(process.argv.slice(2))
+// const colors = require('colors')
+
+// console.log('parametros:', params)
+
+// const nombres = [
+//     'carlos',
+//     'sara',
+//     'Erik',
+//     'Ivonne',
+//     'Alberto',
+//     'Leo',
+//     'Alex',
+//     'Airy',
+//     'freddy',
+//     'Jose'
+//   ]
+
+// const koderName = params.name
+// const exists = nombres.includes(koderName)
+
+// if (exists) {
+//     console.log('El nombre existe'.rainbow)
+// }else {
+//     console.log('EL nombre no se encuentra en la lista')
+// }
+
 const params = require('yargs-parser')(process.argv.slice(2))
 const colors = require('colors')
 
-console.log('parametros:', params)
+const nombreConsola = params.name
 
-const nombres = [
-    'carlos',
-    'sara',
-    'Erik',
-    'Ivonne',
-    'Alberto',
-    'Leo',
-    'Alex',
-    'Airy',
-    'freddy',
-    'Jose'
-  ]
+console.log(`Hola ${nombreConsola}`.rainbow)
 
-const koderName = params.name
-const exists = nombres.includes(koderName)
-
-if (exists) {
-    console.log('El nombre existe'.rainbow)
-}else {
-    console.log('EL nombre no se encuentra en la lista')
-}
